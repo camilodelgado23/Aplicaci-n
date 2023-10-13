634ec86d4e7ebd52dfb9823f88b94e8cba6a22d2
camilodelgado23 <126426982+camilodelgado23@users.noreply.github.com>
2023-10-12 22:46:12 -0500

    ordenesModel.js

--- ordenes/models/ordenesModel.js
+++ ordenes/models/ordenesModel.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
-    port: '3306',
     host: 'db',
+    port: '3306',
     user: 'root',
     password: 'root',
     database: 'almacen'
